694b692fc1b0ee2f9c4803bcae800b8a0e78e804
aframuz <aframuz@tuta.io>
2024-11-13 23:49:49 -0300

    feat: update api response for id out of range

--- src/application/use-cases/GetDucksByIDUseCase.ts
+++ src/application/use-cases/GetDucksByIDUseCase.ts
@@ -1,5 +1,6 @@
 import { IDuckRepository } from "../../domain/repositories/IDuckRespository";
 import { DuckError } from "../../domain/errors/DuckError";
+import { error } from "console";
 
 export class GetDucksByIDUseCase {
     constructor(private readonly duckrepository: IDuckRepository) {}
@@ -7,6 +8,11 @@ export class GetDucksByIDUseCase {
     async execute(id: number): Promise<string[]> {
         try {
             const ducks = await this.duckrepository.getDucksByID(id);
+            
+            if (!ducks.length){
+                throw new error;
+            }
+
             return ducks.map(duck => duck.getUrl());
         } catch (error) {
             throw new DuckError("Failed to get ducks with id " + id, 500);
